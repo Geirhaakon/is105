68e96c42cf443277d7418cb8bc4cb02d0741118a
Glenn Greibesland <glenn@glenng.no>
2015-01-25 14:24:02 +0100

    Fikset noen småfeil og formateringsfeil

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -3,21 +3,21 @@
 #
 #  IS-105 LAB1
 #
-#  lab1.py - kildekode vil inneholde studentenes løsning.
+#  lab1.py - kildekode vil inneholde studentenes lÃ¸ning.
 #         
 #
 #
 import sys
 import psutil
-# Skriv inn fullt navn på gruppemedlemene (erstatte '-' med navn slikt 'Kari Trå')
+# Skriv inn fullt navn pÃ¥ gruppemedlemene (erstatte '-' med navn slikt 'Kari TrÃ¥)
 gruppe = {  'student1': 'Glenn Greibesland' }
 
 #
 #  Oppgave 1
 #    Leke med utskrift 
-#    Skriv ut følgende "ascii art" i en funksjon (erstatte pass)
+#    Skriv ut fÃ¸lgende "ascii art" i en funksjon (erstatte pass)
 #    Funksjonen skal hete ascii_fugl() og skal vÃ¦re uten argumenter og uten returverdier
-#    Den skal skrive ut følgende når den brukes ascii_fugl
+#    Den skal skrive ut fÃ¸lgende nÃ¥ den brukes ascii_fugl
 #
 #       \/_
 #  \,   /( ,/
@@ -38,7 +38,7 @@ def ascii_fugl():
 
 #  Oppgave 2
 #    bitAnd - x&y
-#	 Implementer funksjonen som gjør en "bitwise" AND operasjon (erstatt pass)
+#	 Implementer funksjonen som gjÃ¸r en "bitwise" AND operasjon (erstatt pass)
 #    Eksempel: bitAnd(6, 5) = 4
 #		Forklaring: 6 binÃ¦rt er 110, mens 5 er 101. Hvis vi sammenligner bitvis
 #					1 AND 1 gir 1, 1 AND 0 gir 0 og 0 AND 1 gir 0 => 100 binÃ¦rt
@@ -67,9 +67,9 @@ def bitOr(x, y):
 #  Oppgave 5
 #
 #  Tips:
-#    For å finne desimalverdien til et tegn kan funksjonen ord brukes, for eksempel
+#    For Ã¥ finne desimalverdien til et tegn kan funksjonen ord brukes, for eksempel
 #      ord('A') , det vil gi et tall 65 i ti-tallssystemet
-#    For å formattere 6 i ti-tallssystemet til 00000110 i to-tallssystemet
+#    For Ã¥ formattere 6 i ti-tallssystemet til 00000110 i to-tallssystemet
 #      '{0:08b}'.format(6)
 #      00000110
 #
@@ -77,12 +77,12 @@ def bitOr(x, y):
 #      {} setter en variabel inn i strengen
 #      0 tar variabelen i argument posisjon 0
 #      : legger til formatteringsmuligheter for denne variabelen (ellers hadde den 6 desimalt)
-#      08 formatterer tall til 8 tegn og fuller med nuller til venstre hvis nødvendig
+#      08 formatterer tall til 8 tegn og fuller med nuller til venstre hvis nÃ¸dvendig
 #      b konverterer tallet til dets binÃ¦re representasjon
 #
-#	 Hvilke begrensninger vil en slik funksjon ha? (tips: prøv med bokstaven 'å', f.eks.)
-#	 Forklar resultatet ascii8Bin('å')
-#	 Hvilke faktorer påvirker resultatet? Forklar.
+#	 Hvilke begrensninger vil en slik funksjon ha? (tips: prÃ¸v med bokstaven 'Ã¥', f.eks.)
+#	 Forklar resultatet ascii8Bin('Ã¥')
+#	 Hvilke faktorer pÃ¥virker resultatet? Forklar.
 #
 def ascii8Bin(letter):
 	return '{0:08b}'.format(ord(letter))
@@ -98,11 +98,11 @@ def ascii2Hex(letter):
 #    Eksempel: transferBin("Hi") skriver ut: 
 #                01001000
 #                01101001
-#	 Forklart hver linje i denne funksjonen (hva er list, hva gjør in)
-#	 Skriv selv inn tester ved å bruke assert i funksjonen test()
+#	 Forklart hver linje i denne funksjonen (hva er list, hva gjÃ¸r in)
+#	 Skriv selv inn tester ved Ã¥ bruke assert i funksjonen test()
 #
 def transferBin(string): 
-	l = list(string) # Ikke nødvendig å legge i liste.
+	l = list(string) # Ikke nÃ¸dvendig Ã¥ legge i liste.
 	for c in l:
 		# skriv ut den binÃ¦re representasjon av hvert tegn (bruk ascii8Bin funksjonen din)
 		return "Den binÃ¦re representasjonen for %s:" % c, ascii8Bin(c)
@@ -112,14 +112,14 @@ def transferBinSummary(string):
 	binString = ''
 	for char in string:
 		binString += ascii8Bin(char)
-	return "Hele den binære representasjonen av strengen \'%s\' er: %s" % string, binString
+	return "Hele den binære representasjonen av strengen \'%s\' er: %s" % (string, binString)
 #
 #  Oppgave 7
-#    transferHex - gjør det samme som transferBin, bare skriver ut representasjonen
+#    transferHex - gjÃ¸r det samme som transferBin, bare skriver ut representasjonen
 #					av strengen heksadesimalt (bruk formattering forklart i Oppgave 6)
-#					Skriv gjerne en støttefunksjon ascii2Hex, som representerer et tegn
+#					Skriv gjerne en stÃ¸ttefunksjon ascii2Hex, som representerer et tegn
 #					med 2 heksadesimale tegn
-#    Skriv selv inn tester ved å bruke assert i funksjonen test()
+#    Skriv selv inn tester ved Ã¥ bruke assert i funksjonen test()
 #  
 def transferHex(string):
 	l = list(string)
@@ -130,19 +130,19 @@ def transferHexSummary(string):
 	hexString = ''
 	for char in string:
 		hexString += ascii2Hex(char)
-	return "Hele den heksadesimale representasjonen av strengen \'%s\' er: %s" % string, hexString
+	return "Hele den heksadesimale representasjonen av strengen \'%s\' er: %s" % (string, hexString)
 #
 #
 # Oppgave 8
 # 		Implementer en funksjon unicodeBin, som kan behandle norske bokstaver
 # 		Kravspesifikasjon for denne funksjonen er den samme som for ascii8Bin funksjonen
 def unicodeBin(character):
-	return '{0:08b}'.format(ord(letter.decode('utf8')))
+	return '{0:08b}'.format(ord(character.decode('utf8')))
 
 #
 # Oppgave 9
-# 	Studer python module psutils (må vÃ¦re obs på versjon)
-#   Prøv å finne ut hvordan du kan finne ut og skrive ut følgende informasjon om din 
+# 	Studer python module psutils (mÃ¥ vÃ¦re obs pÃ¥ versjon)
+#   PrÃ¸v Ã¥ finne ut hvordan du kan finne ut og skrive ut fÃ¸lgende informasjon om din 
 #   datamaskin-node:
 #
 # 			Brand and model
@@ -155,7 +155,7 @@ def unicodeBin(character):
 #	Forklar hvorfor man kan / ikke kan finne denne informasjon vha. psutil modulen.
 #	Skriv en funksjon printSysInfo som skriver ut den informasjon som psutil kan finne.
 #	Kan dere skrive en test for denne funksjonen?
-#	Hvilke andre muligheter har man for å finne informasjon om maskinvare i GNU/Linux?
+#	Hvilke andre muligheter har man for Ã¥ finne informasjon om maskinvare i GNU/Linux?
 #
 def printSysInfo():
 	mem = psutil.virtual_memory()
@@ -196,12 +196,12 @@ def test():
 		hexS+=2
 		hexE+=2 
 	
-	assert unicodeBin('å') == '11100101'
+	assert unicodeBin('Ã¥') == '11100101'
 
-	return "Testene er fullført uten feil."
+	return "Testene er fullfÃ¸rt uten feil."
 
 	
 
-# Bruk denne funksjonen for å vise at alle testene er kjørt feilfritt
+# Bruk denne funksjonen for Ã¥ vise at alle testene er kjÃ¸rt feilfritt
 print test()
 		
