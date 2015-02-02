80edba5d43f2fe6b154b9840d77f3b742f97c5aa
GGreibesland <glenn@glenng.no>
2015-02-02 22:57:36 +0100

    Almost complete client/server application for converting lower to uppercase. Started working on roman numerals part

---
+++ Lab3/lab3client.py
@@ -0,0 +1,108 @@
+'''
+Created on 2. feb. 2015
+
+@author: GGreibesland
+'''
+from socket import *
+import json
+
+serverName = 'localhost' # Server to connect to
+serverPort = 12000 # Remote server port
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+
+# Receive list of functions from server
+def getSupportedFuncFromServer():
+    command = json.dumps(('system', 'getFunctions'))
+    clientSocket.sendto(command,(serverName, serverPort))
+    result, serverAddress = clientSocket.recvfrom(2048)
+    return json.loads(result)
+
+def printSupportedFunc():
+    print 'This application supports %i commands:' % len(supportedFunc)
+    for f in supportedFunc:
+        cmd = f.split(':')
+        print cmd[0] + ':\t', cmd[1]
+
+def getSupportedFuncCommands():
+    funcs = ''
+    i=0
+    for f in supportedFunc:
+        func = f.split(':')
+        if i != len(supportedFunc):
+            funcs += func[0] + ', '
+        i += 1
+    return funcs
+
+def getCommandCategoryMap():
+    d = dict()
+    for cmd in supportedFunc:
+        c = cmd.split(':')
+        d[c[0]] = c[2]
+    return d
+
+def upperConvert(fun):
+    # Ask user for input, decode, encode, then send.
+    message = raw_input('Input lowercase sentence:').decode('utf8').encode('utf8')
+    # Prepend the function to run in the message followed by a colon
+    message = json.dumps((fun, message))
+    clientSocket.sendto(message,(serverName, serverPort))
+    msgFromServer, serverAddress = clientSocket.recvfrom(2048)
+    jsonFromServer = json.loads(msgFromServer)
+    print '==========================\nReply from server:'
+    print 'Characters in uppercase: ',
+    if fun == 'u':
+        print jsonFromServer
+    if fun in 'db':
+        print jsonFromServer[0]
+        print '\nDecimal position in uppercase: ',
+        print jsonFromServer[1]
+    if fun == 'b':
+        print '\nBinary representation in uppercase: ',
+        print jsonFromServer[2]
+    
+def roman(fun):
+    pass
+
+def sysFunctions(fun):
+    pass
+
+def shutdownServer():
+    message = json.dumps(('system', 'shutdown'))
+    clientSocket.sendto(message,(serverName, serverPort))
+
+#Save a list of supported functions for later use
+supportedFunc = getSupportedFuncFromServer()
+commandCategoryMap = getCommandCategoryMap()
+
+
+
+# Print welcome and a list of supported commands
+print 'Hi, welcome to this lab3 test program.'
+printSupportedFunc()
+# Start main program loop
+while True:
+    
+    # Ask user what function to use
+    fun = raw_input('Please select which function you want to use (' + getSupportedFuncCommands() + '): ').decode('utf8').encode('utf8')
+    # Check if command is valid
+    if fun in (getSupportedFuncCommands()) and fun != '':
+        # Some special commands, like exit, shutdown and help
+        if fun == 'e': break
+        if fun == 's':
+            shutdownServer()
+            break
+        if fun == 'h': printSupportedFunc()
+        
+        # Other commands, main functions of the program
+        if commandCategoryMap[fun] == 'upper': upperConvert(fun)
+        if commandCategoryMap[fun] == 'roman': roman(fun)
+        if commandCategoryMap[fun] == 'system': sysFunctions(fun)
+
+        
+    else:
+        print 'Sorry, that is not a supported command.'
+
+clientSocket.close()
+        
+print 'Goodbye'
---
+++ Lab3/lab3server.py
@@ -0,0 +1,96 @@
+'''
+Created on 2. feb. 2015
+
+@author: GGreibesland
+'''
+from socket import *
+from lab3supportmodule import *
+import json
+
+serverPort = 12000 # Server port to bind to
+serverHostIP = 'localhost' # IP or hostname to bind server to
+debug = True
+
+
+def lab3debug(msg):
+    if debug:
+        print msg
+        
+
+# List of supported functions.
+# I created a regular list, because of trouble with converting a MAP to json.
+# The list is separated by :
+# 1st space is for the command
+# 2nd space is the description of the command
+# 3rd space is for command category
+supportedFunc = ('v: turn off or on verbose mode:system',
+                 'r: do math operations on roman numerals:roman',
+                 'u: convert string to uppercase by using the upper function:upper',
+                 'b: convert a string to uppercase by manipulating binary representation of character:upper',
+                 'd: convert a string to uppercase by manupulating decimal representation of character:upper',
+                 'e: exit client program:system',
+                 's: shut down server program and exit client:system',
+                 'h: show this help text:help')
+# List of supported roman functions
+# Same idea as the above list.
+# 1st space is the function name
+# 2nd space is the description
+supportedRomanFunc = ('toInt:Convert roman to int',
+                      'toRoman:Convert int to roman',
+                      'add:Add two roman numerals',
+                      'subtract:Subtract two roman numerals',
+                      'mult:multiply two roman numerals',
+                      'divide:divide two roman numerals')
+
+
+
+# Type of socket to set up
+serverSocket = socket(AF_INET,SOCK_DGRAM)
+# Set up the listener
+serverSocket.bind((serverHostIP,serverPort))
+print 'The server is ready to receive'
+
+def startServer():
+    # Loop which listens for data from a client.
+    while 1:
+        message, clientAddress = serverSocket.recvfrom(2048)
+        messagefromClient = json.loads(message)
+        decodedMessage = messagefromClient[1].decode('utf-8')
+        command = messagefromClient[0].decode('utf-8')
+        lab3debug('Message from client is: ' + message)
+        lab3debug('Command is:' + command)
+        
+        if command == unicode('system') and decodedMessage == unicode('getFunctions'):
+            reply = json.dumps(supportedFunc)
+        if command == unicode('system') and decodedMessage == unicode('shutdown'):
+            print 'Exiting server application'
+            break
+        if command == unicode('u'):
+            reply = decodedMessage.upper().encode('utf-8')
+            reply = json.dumps((reply))
+        if command == unicode('d'):
+            charReply = reply = ''
+            for l in decodedMessage:
+                reply += chr(charToUpperDec(l))
+                charReply += str(charToUpperDec(l)) + ' '
+            reply = json.dumps((reply, charReply))
+        if command == unicode('b'):
+            charReply = binReply = reply = ''
+            for l in decodedMessage:
+                reply += chr(charToUpperBin(l))
+                charReply += str(charToUpperBin(l)) + ' '
+                binReply += unicodeBin(chr(charToUpperBin(l))) + ' '
+            reply = json.dumps((reply, charReply, binReply))
+            
+        # Roman functions
+        if command == unicode('r'):
+            reply = json.dumps(supportedRomanFunc)
+            
+            # Entery a new loop where we accept only roman commands
+
+        lab3debug('Message to client: ' + reply)
+        serverSocket.sendto(reply, clientAddress)
+
+
+
+startServer()
